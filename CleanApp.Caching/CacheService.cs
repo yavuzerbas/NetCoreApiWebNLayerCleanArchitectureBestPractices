271f4f119f989c78b5fcb6d3220f543b4184a5a2
yavuzerbas <yavuz0erbas@gmail.com>
2024-10-18 23:00:39 +0300

    Add memory cache

---
+++ CleanApp.Caching/CacheService.cs
@@ -0,0 +1,36 @@
+﻿using CleanApp.Application.Contracts.Caching;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace CleanApp.Caching
+{
+    public class CacheService(IMemoryCache memoryCache) : ICacheService
+    {
+        public Task<T?> GetAsync<T>(string cacheKey)
+        {
+            /*if (memoryCache.TryGetValue(cacheKey, out T cacheItem)) return Task.FromResult(cacheItem);
+
+            return Task.FromResult(default(T));*/
+
+            return Task.FromResult(memoryCache.TryGetValue(cacheKey, out T cacheItem) ? cacheItem : default(T));
+        }
+
+        public Task AddAsync<T>(string cacheKey, T value, TimeSpan expireTimeSpan)
+        {
+            var cacheOptions = new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = expireTimeSpan
+            };
+
+            memoryCache.Set(cacheKey, value, cacheOptions);
+
+            return Task.CompletedTask;
+
+        }
+
+        public Task RemoveAsync(string cacheKey)
+        {
+            memoryCache.Remove(cacheKey);
+            return Task.CompletedTask;
+        }
+    }
+}
