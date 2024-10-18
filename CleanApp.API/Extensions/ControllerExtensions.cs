f75dd5e7213758e1d3a744b74a27f096225d9958
yavuzerbas <yavuz0erbas@gmail.com>
2024-10-18 23:31:58 +0300

    Refactor Program.cs

---
+++ CleanApp.API/Extensions/ControllerExtensions.cs
@@ -0,0 +1,18 @@
+﻿using CleanApp.API.FIlters;
+
+namespace CleanApp.API.Extensions
+{
+    public static class ControllerExtensions
+    {
+        public static IServiceCollection AddControllerWithFiltersExt(this IServiceCollection services)
+        {
+            services.AddControllers(options =>
+            {
+                options.Filters.Add<FluentValidationFilter>();
+                options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true;
+            });
+            services.AddScoped(typeof(NotFoundFilter<,>));
+            return services;
+        }
+    }
+}
--- CleanApp.API/Program.cs
+++ CleanApp.API/Program.cs
@@ -1,44 +1,18 @@
-using CleanApp.API.ExceptionHandlers;
-using CleanApp.API.FIlters;
-using CleanApp.Application.Contracts.Caching;
+using CleanApp.API.Extensions;
 using CleanApp.Application.Extensions;
-using CleanApp.Caching;
 using CleanApp.Persistence.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers(options =>
-{
-    options.Filters.Add<FluentValidationFilter>();
-    options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true;
-});
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddControllerWithFiltersExt().AddSwaggerGenExt().AddExceptionHandlerExt().AddCachingExt();
 
 builder.Services.AddRepositories(builder.Configuration).AddServices(builder.Configuration);
 
-builder.Services.AddScoped(typeof(NotFoundFilter<,>));
-builder.Services.AddExceptionHandler<CriticalExceptionHandler>();
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-builder.Services.AddMemoryCache();
-builder.Services.AddSingleton<ICacheService, CacheService>();
-
 var app = builder.Build();
-app.UseExceptionHandler(x => { });
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseConfigurePipelineExt();
 
 app.MapControllers();
 
